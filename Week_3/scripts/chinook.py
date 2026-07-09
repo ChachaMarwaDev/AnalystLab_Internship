d07e811cdfbe33a7827289f2243439e43060ccd3
Chacha Marwa <chachamarwa003@gmail.com>
2026-07-09 06:14:16 +0300

    Add Chinook SQLite database and initial script for data analysis

---
+++ Week_3/scripts/chinook.py
@@ -0,0 +1,13 @@
+# %%
+import duckdb
+
+# Install and load the sqlite extension (only needed once)
+# I have run it so I will comment it out
+# duckdb.sql("INSTALL sqlite; LOAD sqlite;")
+# %%
+# Attach the Chinook database
+duckdb.sql("ATTACH 'Chinook_Sqlite.sqlite' AS chinook (TYPE sqlite)")
+
+# Test it - list all tables
+print(duckdb.sql("SHOW ALL TABLES").df())
+# %%
