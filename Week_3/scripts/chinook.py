a90349f83fa04b69183b27ed68789570c78bcc12
Chacha Marwa <chachamarwa003@gmail.com>
2026-07-09 06:15:30 +0300

    Enhance chinook.py with additional test queries and formatting

--- Week_3/scripts/chinook.py
+++ Week_3/scripts/chinook.py
@@ -4,10 +4,18 @@ import duckdb
 # Install and load the sqlite extension (only needed once)
 # I have run it so I will comment it out
 # duckdb.sql("INSTALL sqlite; LOAD sqlite;")
+
 # %%
 # Attach the Chinook database
 duckdb.sql("ATTACH 'Chinook_Sqlite.sqlite' AS chinook (TYPE sqlite)")
 
 # Test it - list all tables
 print(duckdb.sql("SHOW ALL TABLES").df())
+
+# %%
+# Test
+result = duckdb.sql("""
+    SELECT * FROM chinook.Customer LIMIT 5
+""").df()
+print(result)
 # %%
