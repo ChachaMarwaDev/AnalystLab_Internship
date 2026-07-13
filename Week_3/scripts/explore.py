1b978bc195a3b34e8657076901875454116e5814
Chacha Marwa <chachamarwa003@gmail.com>
2026-07-13 05:31:44 +0300

    Add .gitignore file to exclude Python bytecode files and create explore.py for database exploration queries

---
+++ Week_3/scripts/explore.py
@@ -0,0 +1,13 @@
+# %%
+from connect import get_connection, run_query
+
+con = get_connection()
+
+# %%
+print(run_query(con, "SHOW ALL TABLES"))
+
+# %%
+print(run_query(con, "SELECT DISTINCT country FROM chinook.Customer"))
+# %%
+with open("queries.sql") as f:
+    print(run_query(con, f.read()))
