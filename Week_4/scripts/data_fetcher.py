1db98d03ec7c2cfcf6d453dc937142d3d06ba4c7
Chacha Marwa <chachamarwa003@gmail.com>
2026-07-15 09:28:07 +0300

    Refactor exploratory data analysis section in data_fetcher.py for clarity and organization

--- Week_4/scripts/data_fetcher.py
+++ Week_4/scripts/data_fetcher.py
@@ -11,3 +11,18 @@ df = pd.read_csv("../data/Sample_Superstore.csv", encoding='cp1252')
 print(df.head(2))
 
 # %%
+"""
+EXPLORATORY DATA ANALYSIS
+Shape is (9994, 21)
+Zero nulls in the table
+"""
+# print(df.shape)
+# print(df.columns)
+# print(df.isnull().sum())
+# print(df.dtypes)
+
+# print(df['Order Date'].dtype)
+# print(df['Order Date'].head())
+
+# print(df.info)
+
