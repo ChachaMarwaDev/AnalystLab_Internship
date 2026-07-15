86a97f3399f9f61f39931a243a85f08549d9cf68
Chacha Marwa <chachamarwa003@gmail.com>
2026-07-15 09:04:44 +0300

    Update data fetcher script to download Superstore dataset from Kaggle

--- Week_4/scripts/data_fetcher.py
+++ Week_4/scripts/data_fetcher.py
@@ -1,3 +1,9 @@
+# %%
+import kagglehub as ka
+import pandas as pd
+import numpy as np
 
+link = "https://www.kaggle.com/datasets/vivek468/superstore-dataset-final"
+path = ka.dataset_download("vivek468/superstore-dataset-final", output_dir="../data")
 
-link = "https://github.com/CSSEGISandData/COVID-19"
+# %%
