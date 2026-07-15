8bdcaa3979782ba21dc90e14f444010896320ac6
Chacha Marwa <chachamarwa003@gmail.com>
2026-07-15 09:07:54 +0300

    Add CSV reading functionality to data_fetcher.py

--- Week_4/scripts/data_fetcher.py
+++ Week_4/scripts/data_fetcher.py
@@ -7,3 +7,4 @@ link = "https://www.kaggle.com/datasets/vivek468/superstore-dataset-final"
 path = ka.dataset_download("vivek468/superstore-dataset-final", output_dir="../data")
 
 # %%
+df = pd.read_csv()
