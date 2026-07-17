cf99b128885449c684247f2612cfb9de30843724
Chacha Marwa <chachamarwa003@gmail.com>
2026-07-17 04:46:55 +0300

    Update fetcher.py to load the correct dataset for Telco Customer Churn analysis

--- Week_5/scripts/fetcher.py
+++ Week_5/scripts/fetcher.py
@@ -7,5 +7,17 @@ dataset = "blastchar/telco-customer-churn"
 path = ka.dataset_download(dataset, output_dir="../data")
 
 # %%
-df = pd.read_csv("../data/Sample_Superstore.csv", encoding='cp1252')
+df = pd.read_csv("../data/Telco_Churn.csv", encoding='cp1252')
 print(df.head(2))
+
+# %%
+# print(df.shape)
+# print(df.columns)
+# print(df.isnull().sum())
+# print(df.dtypes)
+
+# print(df['TotalCharges'].dtype)
+# print(df['TotalCharges'].head())
+
+# print(df.info)
+# %%
