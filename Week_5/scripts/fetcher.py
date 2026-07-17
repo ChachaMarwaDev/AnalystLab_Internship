4067840ca00b0c30269b98335dc6ffea46ab8188
Chacha Marwa <chachamarwa003@gmail.com>
2026-07-17 04:46:36 +0300

    Add dataset download and CSV loading in fetcher.py
    - Updated fetcher.py to include dataset download from Kaggle.
    - Added code to read the Sample Superstore CSV file with specified encoding.
    - Included a print statement to display the first two rows of the dataframe.

--- Week_5/scripts/fetcher.py
+++ Week_5/scripts/fetcher.py
@@ -3,3 +3,9 @@ import kagglehub as ka
 import pandas as pd
 
 data_link = "https://www.kaggle.com/datasets/blastchar/telco-customer-churn"
+dataset = "blastchar/telco-customer-churn"
+path = ka.dataset_download(dataset, output_dir="../data")
+
+# %%
+df = pd.read_csv("../data/Sample_Superstore.csv", encoding='cp1252')
+print(df.head(2))
